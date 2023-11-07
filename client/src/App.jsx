dda92dec31abaec229307c6b04270e43125bdf38
dangchinh9707 <chinhdtph29556@fpt.edu.vn>
2023-11-08 01:59:47 +0700

    add client server done

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='text-green-600'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
